537215630d013fca31c0ad5a3425f15a3424e70b
agent86ix <agent86ix@gmail.com>
2020-09-26 17:02:03 +0000

    Attempt to auto-detect different SD mount dirs

--- Upgrade/flash_bootloader.sh
+++ Upgrade/flash_bootloader.sh
@@ -1,13 +1,18 @@
 #!/bin/sh
 set -e
 
+BACKUP_DIR=$1
+
+echo "Using backup directory: $BACKUP_DIR"
+
 echo "Changing to upgrade directory."
 cd /tmp/Upgrade
 echo "Verifying file integrity."
 md5sum -c wyzecam_v2_stock_bootloader.bin.md5
 echo "Backing up current bootloader."
-dd if=/dev/mtd0 of=/media/mmcblk0p1/wyze-cam-pan-fix-old-bootloader.bin
+dd if=/dev/mtd0 of=$BACKUP_DIR/wyze-cam-pan-fix-old-bootloader.bin
 echo "Erasing bootloader flash."
 flash_eraseall /dev/mtd0
 echo "Installing replacement bootloader."
 dd if=wyzecam_v2_stock_bootloader.bin of=/dev/mtd0
+reboot
--- Upgrade/upgraderun.sh
+++ Upgrade/upgraderun.sh
@@ -1,6 +1,19 @@
 #!/bin/sh
 set -e
 
-ls -al /tmp/Upgrade
-cat /tmp/Upgrade/flash_bootloader.sh
-sh /tmp/Upgrade/flash_bootloader.sh &> /media/mmcblk0p1/wyze-cam-pan-fix.log
+if [ -d "/media/mmcblk0p1/" ]
+then
+    BACKUP_DIR=/media/mmcblk0p1/
+elif [ -d "/media/mmc/" ]
+then
+    BACKUP_DIR=/media/mmc/
+else
+    BACKUP_DIR=`mount | grep mmc | grep -v -e /root -e /etc -e /bin | cut -d ' ' -f 3`
+fi
+
+if [ ! -d $BACKUP_DIR ]
+then
+    exit 1
+fi
+
+sh /tmp/Upgrade/flash_bootloader.sh $BACKUP_DIR &> $BACKUP_DIR/wyze-cam-pan-fix.log
